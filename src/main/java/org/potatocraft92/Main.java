d09e7ba1fa3fb2dd83f9451968d4a9677942de89
TuDouNi92 <potato-craft@hotmail.com>
2026-02-03 00:17:16 +0800

    Update Dependencies and fix malfunction on share

--- src/main/java/org/potatocraft92/Main.java
+++ src/main/java/org/potatocraft92/Main.java
@@ -1,17 +1,37 @@
 package org.potatocraft92;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mvplugins.multiverse.inventories.MultiverseInventoriesApi;
+import org.mvplugins.multiverse.inventories.profile.group.WorldGroup;
+import org.mvplugins.multiverse.inventories.profile.group.WorldGroupManager;
+import org.mvplugins.multiverse.inventories.share.Sharables;
 import org.potatocraft92.share.CuriosSharable;
-
 import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        MultiverseInventoriesApi.get().getWorldGroupManager().recalculateApplicableShares();
-        getLogger().info("Enabled WMFC,loaded:" + CuriosSharable.CURIO_SHARES.toString());
+        createCurioGroup();
+        getLogger().info("Enabled WMFC");
+    }
+
+    private static void createCurioGroup() {
+        WorldGroupManager manager = MultiverseInventoriesApi.get().getWorldGroupManager();
+        WorldGroup newGroup = manager.newEmptyGroup("curios");
+        if (newGroup != null) {
+            newGroup.addWorld("world");
+            newGroup.addWorld("DIM1");
+            newGroup.addWorld("DIM-1");
+            newGroup.getShares().addAll(Sharables.fromSharables(CuriosSharable.CURIO_SHARES));
+            manager.updateGroup(newGroup);
+        } else {
+            WorldGroup curiosGroup = manager.getGroup("curios");
+            curiosGroup.getShares().addAll(Sharables.fromSharables(CuriosSharable.CURIO_SHARES));
+            manager.updateGroup(curiosGroup);
+        }
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),"mvinv reload");
     }
 
     @Override
